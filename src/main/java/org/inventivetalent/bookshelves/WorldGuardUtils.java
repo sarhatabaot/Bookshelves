71ef9986305aa4582002db04c7d096587ad9e4d7
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-20 13:10:30 +0200

    Moved to proper maven directory setup

--- src/main/java/org/inventivetalent/bookshelves/WorldGuardUtils.java
+++ src/main/java/org/inventivetalent/bookshelves/WorldGuardUtils.java
@@ -5,14 +5,11 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import com.sk89q.worldguard.protection.managers.RegionManager;
-import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.inventivetalent.bookshelves.Bookshelves;
 
 import java.util.Optional;
 
