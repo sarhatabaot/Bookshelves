3d48d63640a93c320205e21361e027e2a0dc7bc0
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-20 13:14:00 +0200

    Added jetbrains annotations

--- src/main/java/org/inventivetalent/bookshelves/WorldGuardUtils.java
+++ src/main/java/org/inventivetalent/bookshelves/WorldGuardUtils.java
@@ -10,29 +10,32 @@ import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
 public class WorldGuardUtils {
+    private static final StateFlag stateFlag = new StateFlag("bookshelf-access", false);
 
-    private static final StateFlag stateFlag = new StateFlag( "bookshelf-access", false);
+    private WorldGuardUtils(){
+        throw new UnsupportedOperationException();
+    }
 
     public static void registerBookshelfAccessFlag() {
-        try
-        {
-            WorldGuard.getInstance().getFlagRegistry().register( stateFlag );
-        }
-        catch (FlagConflictException ex)
-        {
+        try {
+            WorldGuard.getInstance().getFlagRegistry().register(stateFlag);
+        } catch (FlagConflictException ex) {
             Bookshelves.instance.getLogger().warning("The flag '" + stateFlag.getName() + "' is already registered by another plugin. Conflicts might occur!");
         }
     }
 
-    public static boolean isAllowedToAccess(Player player, Block block) {
-        if (player.hasPermission("worldguard.region.bypass." + block.getWorld().getName())) { return true; }
+    public static boolean isAllowedToAccess(@NotNull Player player, @NotNull Block block) {
+        if (player.hasPermission("worldguard.region.bypass." + block.getWorld().getName())) {
+            return true;
+        }
 
         WorldGuard worldGuard = WorldGuard.getInstance();
-        Optional<RegionManager> regionManager = Optional.ofNullable( worldGuard.getPlatform().getRegionContainer().get(BukkitAdapter.adapt(block.getWorld())) );
+        Optional<RegionManager> regionManager = Optional.ofNullable(worldGuard.getPlatform().getRegionContainer().get(BukkitAdapter.adapt(block.getWorld())));
 
         LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
         BlockVector3 location = BukkitAdapter.adapt(block.getLocation()).toVector().toBlockPoint();
